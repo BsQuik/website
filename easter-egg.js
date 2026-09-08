79773df939e8ae86abb93f86a8ab058e74956181
Quik <tryte239@gmail.com>
2026-09-08 15:45:24 +0200

    Add new Easter egg for 'hi' command

--- easter-egg.js
+++ easter-egg.js
@@ -237,6 +237,7 @@
     { word: 'minecraft', run: function () { spawnBlockRain(); showToast(window.t ? window.t('minecraftToast') : 'Achievement get: Blocco di Diamante 💎'); } },
     { word: 'brawl', run: function () { spawnConfetti(); showToast(window.t ? window.t('brawlToast') : 'Super attivata! ⭐'); } },
     { word: 'gg', run: function () { showToast(window.t ? window.t('ggToast') : 'GG! 🏆'); } }
+    { word: 'hi', run: function () { showToast(window.t ? window.t('ggToast') : 'hi! 🏆'); } }
   ];
   var typedBuffer = '';
   var TYPED_BUFFER_MAX = 20;
